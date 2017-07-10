8ff9d6048298f53589d6ca2a5bcfeb68fd9144d4
Sarah Herr <sarah.herr@hotmail.com>
2017-07-10 10:12:00 -0400

    change(routing)

--- browser/root.jsx
+++ browser/root.jsx
@@ -16,11 +16,11 @@ export default function Root () {
     <Provider store={store}>
       <Router history={browserHistory}>
         <Route path='/' component={AppContainer}>
+        <Route path='/' component={About} />
         <Route path='/projects' component={Test} />
-        <Route path='/about' component={About} />
         <Route path='/contact' component={Contact} />
         <Route path='/home' component={Home} />
-        <IndexRedirect to='/about' />
+        <IndexRedirect to='/' />
         </Route>
       </Router>
     </Provider>
